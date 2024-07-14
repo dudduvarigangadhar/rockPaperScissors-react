1248d3331caf12718b0221afef3410c333ddb632
gangadhar <141738423+dudduvarigangadhar@users.noreply.github.com>
2024-07-14 11:38:06 +0530

    Update styledComponents.js

--- src/component/GameComponent/styledComponents.js
+++ src/component/GameComponent/styledComponents.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
 
 export const ImgTag = styled.img`
-  height: 200px;
-  margin-right: 30px;
+  height: 170px;
+  width: 170px;
+  //   margin-right: 30px;
+  cursor: pointer;
+`
+export const OptionsList = styled.li`
+  list-style-type: none;
+  display: flex;
+`
+export const GameOptionButton = styled.button`
+  border: none;
+  background-color: transparent;
+  outline: none;
   cursor: pointer;
 `
